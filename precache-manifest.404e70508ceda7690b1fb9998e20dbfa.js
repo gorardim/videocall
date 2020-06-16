7ef8f2b07514f91a92bc2e425716b3b1185155e2
Fayez Ahmed <fayez97@gmail.com>
2020-06-16 21:58:42 +0600

    first commit

---
+++ precache-manifest.404e70508ceda7690b1fb9998e20dbfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8de68dcd7cc067f139371f3ef20d7ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53636b77749b30ce750c",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "e293bb9e73a9b71e9938",
+    "url": "/static/js/2.42a051d2.chunk.js"
+  },
+  {
+    "revision": "d0ed0e93fdba385200229b33bb0108ea",
+    "url": "/static/js/2.42a051d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53636b77749b30ce750c",
+    "url": "/static/js/main.2b9d371f.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
